bf7a83330ae7473437ce483299228631d30e0d0e
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-09 15:12:52 +0300

    This is not right.

--- Lesson 2/hackxercise4.py
+++ Lesson 2/hackxercise4.py
@@ -1,5 +1,16 @@
 def get_prg(plaintext_size, k):
-    pass # return keystream
+    new_k = []
+    k = list(k)
+    i = j = 0
+    for x in range(plaintext_size):
+        i = (i + 1) % 32
+        j = (j + ord(k[i])) % 32
+        k[i], k[j] = k[j], k[i]  # swap
+        new_k.append(k[(k[i] + k[j]) % 32])
 
 def fake_rc4(plaintext, keystream):
     pass # return ciphertext
+
+
+
+get_prg(32, "abcdeabcdeabcdeabcdeabcdeabcde12")
