7807ddde8073c77b08174ca6d7dd794395df04db
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-09 12:17:40 +0300

    Create the brute force function and add constant values.

--- Lesson 2/hackxercise2.py
+++ Lesson 2/hackxercise2.py
@@ -1,4 +1,6 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+K_MAX_VALUE = 25
+K_MIN_VALUE = 0
 
 def encrypt(plaintext, k):
     ciphertext = []
@@ -12,6 +14,18 @@ def decrypt(ciphertext, k):
     return encrypt(ciphertext, -k)
     
 def brute_force(ciphertext):
-    pass #print all (plaintext, k) possibilities and copy the right one to the Edx platform
+    """Print all the results from the brute force"""
+    for k in range(K_MIN_VALUE, K_MAX_VALUE + 1):
+        print(decrypt(ciphertext, k))
+        
+    """The right value of k"""
+    print("-------------------------------------------")
+    k_right_value = 17
+    print("The plaintext is:")
+    print(decrypt(ciphertext, k_right_value))
+    print("The right k value is:")
+    print(k_right_value)
+    print("-------------------------------------------")
+    return k_right_value
     
 brute_force("kyvtrmrcipnzccrkkrtbwifdkyvefikynvjkrkeffe")
