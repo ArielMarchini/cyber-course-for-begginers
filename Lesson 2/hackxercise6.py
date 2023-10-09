628cffd660e8e80ea24095800c801adb421942db
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-09 17:45:38 +0300

    The code we get.

---
+++ Lesson 2/hackxercise6.py
@@ -0,0 +1,9 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+def aes_encrypt(plaintext, k):
+    pass # return iv + ciphertext (in bytes)
+
+def aes_decrypt(ciphertext, k):
+    pass # return plaintext (in 'latin1')
