0dbf8810c64706edaae0c1fd8e022d75c5b38dd8
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-09 14:02:49 +0300

    Created the function that encrypt and created the ciphertext.

--- Lesson 2/hackxercise3.py
+++ Lesson 2/hackxercise3.py
@@ -1,2 +1,6 @@
 def encrypt(plaintext, k):
-    pass # Do stuff and return ciphertext
+    plaintext_ascii = [ord(c) for c in plaintext]
+    k_ascii = [ord(c) for c in k]
+    xor_chars_results = list(chr(a^b) for a,b in zip(plaintext_ascii,k_ascii))
+    ciphertext ="".join([str(i) for i in xor_chars_results])
+    return ciphertext
