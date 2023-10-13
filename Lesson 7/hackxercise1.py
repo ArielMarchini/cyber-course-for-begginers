c6119235ffa328c11a22e69be8308b54072efd25
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-13 12:58:02 +0300

    Create publish function for both classes.

---
+++ Lesson 7/hackxercise1.py
@@ -0,0 +1,43 @@
+import random
+
+
+p = 283
+g = 47
+
+
+class Alice:
+
+    def __init__(self):
+        self.a = random.randint(1, p)
+
+    def publish(self):
+        return pow(g, self.a) % p
+
+    def compute_secret(self, gb):
+        pass # TODO
+
+
+class Bob:
+
+    def __init__(self):
+        self.b = random.randint(1, p)
+
+    def publish(self):
+        return pow(g, self.b) % p
+
+    def compute_secret(self, ga):
+        pass # TODO
+
+
+alice = Alice()
+bob = Bob()
+print('Alice selected: %s' % alice.a)
+print('Bob selected: %s' % bob.b)
+ga = alice.publish()
+gb = bob.publish()
+print('Alice published: %s' % ga)
+print('Bob published: %s' % gb)
+sa = alice.compute_secret(gb)
+sb = bob.compute_secret(ga)
+print('Alice computed: %s' % sa)
+print('Bob computed: %s' % sb)
