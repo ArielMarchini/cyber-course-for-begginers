8a8717a4321ca350fda48ca015724048826cc175
ArielMarchini <90059396+ArielMarchini@users.noreply.github.com>
2023-10-16 15:03:19 +0300

    Update hackxercise2.py

--- Course Exam 2/hackxercise2.py
+++ Course Exam 2/hackxercise2.py
@@ -12,21 +12,15 @@ g = 47
 class SecureChannel:
 
     def __init__(self, p, g):
-        self.a = random.randint(2, p - 1)
+        self.a = random.randint(1, p)
 
     def publish(self):
         return pow(g, self.a) % p
 
     def encrypt(self, gb, text):
-        print(text)
         s = str(pow(gb, self.a) % p)
-        s_hash = hashlib.sha256(s.encode()).hexdigest()[:16]
-        iv = "gk5nsxxi98ql09jj"
-        decipher = AES.new(s_hash, AES.MODE_CBC, iv)
-        print(decipher)
-        # print(iv + decipher)
-        # return iv + decipher
-        
-s1 = SecureChannel(p, g)
-s2 = SecureChannel(p, g)
-s1.encrypt(s2.publish(), "1234567891234567")
+        iv = "asdfghjklzxcvbnm"
+        hash = hashlib.sha256(s.encode()).hexdigest()[:16]
+        c = AES.new(hash, AES.MODE_CBC, iv)
+        return c
+
